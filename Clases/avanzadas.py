4ff83bc5b2258be5933221e52ace69e1b344d33b
AlexGove <193007-a@iberoleon.edu.mx>
2025-09-04 12:23:29 -0600

    Se Completo avanzadaz.py con el metodo raiz y se agrego menu en main.py

--- Clases/avanzadas.py
+++ Clases/avanzadas.py
@@ -23,5 +23,15 @@ class Operaciones:
     def potencia(self):
         self.resultado = "La potencia de " + str(self.num1) + " elevado a " + str(self.num2) + " es igula a " + str(self.num1**self.num2)
 
+
+    def raiz(self):
+        if self.num2 == 0:
+            self.resultado = "Error: el índice de la raíz no puede ser 0"
+        elif self.num1 < 0 and self.num2 % 2 == 0:
+            self.resultado = "Error: no se puede calcular raíz par de un número negativo"
+        else:
+            self.resultado = f"La raíz {self.num2}-ésima de {self.num1} es igual a {self.num1 ** (1 / self.num2)}"
+        return self.resultado
+    
     def mostrarResultado(self):
         print(self.resultado)
